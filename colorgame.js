fa7ae2cf71c94ecc460eb9b478690fdd73c86940
cmsegui <cmsegui@yahoo.com>
2017-11-01 17:33:57 -0400

    added logic to make random color and to show in different squares on every new game

--- colorgame.js
+++ colorgame.js
@@ -1,47 +1,60 @@
-var colors = [
-    "rgb(255, 0, 0)", 
-    "rgb(25, 125, 75)", 
-    "rgb(125, 0, 200)",
-    "rgb(50, 255, 234)",
-    "rgb(0, 0, 125)",
-    "rgb(150, 200, 0)"
-];
-
+var colors = generateRandomColors(6);
 var squares = document.querySelectorAll(".square");
-var pickedColor = pickedColor();
-var colorDisplay = document.querySelector("#colorDisplay");
+var pickedColor = pickColor();
+var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
-for(var i = 0; i < squares.length; i++) {
-    //add initial colors to squares
-    squares[i].style.backgroundColor = colors[i];
+for(var i = 0; i < squares.length; i++){
+	// add initial colors to squares
+	squares[i].style.background = colors[i];
 
-    //add event listeners to squares
-    squares[i].addEventListener("click", function () {
-        //grab color of clicked squares
-        var clickedColor = this.style.backgroundColor;
-        //compare color to pickedColor
-        if (clickedColor === pickedColor) {
-            messageDisplay.textContent = "Correct";
-            changeColors(clickedColor);
-        } else {
-            this.style.backgroundColor = "#232323";
-            messageDisplay.textContent = "Try Again";
-        };
-    });
-};
+	//add click listeners to squares
+	squares[i].addEventListener("click", function() {
+		//grab color of clicked squares
+		var clickedColor = this.style.background;
+		//compare color to pickedColor
+		if(clickedColor === pickedColor) {
+			messageDisplay.textContent = "Correct!";
+			changeColors(clickedColor);
+			h1.style.background = clickedColor;
+		} else {
+			this.style.background = "#232323";
+			messageDisplay.textContent = "Try Again";
+		}
+	});
+}
 
 function changeColors(color) {
-    //loop through all squares
-  for(var i = 0; i < squares.length; i++) {
-      //change each color to match given color
-      squares[i].style.backgroundColor = color;
-  }
-};
+	//loop through all squares
+	for(var i = 0; i < squares.length; i++) {
+		//change each color to match given color
+		squares[i].style.background = color;
+	}
+}
 
-function pickedColor() {
-    var random = Math.floor(Math.random() * colors.length);
-    return colors[random];
+function pickColor() {
+	var random = Math.floor(Math.random() * colors.length);
+	return colors[random];
+}
+
+function generateRandomColors(num) {
+	//make an array
+	var arr = [];
+	//add num random colors to arr
+	for(var i = 0; i < num; i++) {
+		//get random color and push into arr
+		arr.push(randomColor());
+	}
+	return arr;
+}
+
+function randomColor() {
+	//pick a random number for red, green, blue
+	var r = Math.floor(Math.random() * 256);
+	var g = Math.floor(Math.random() * 256);
+	var b = Math.floor(Math.random() * 256);
+	return "rgb(" + r + ", " + g + ", " + b + ")";
 }
